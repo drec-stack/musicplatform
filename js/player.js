228b5be8740739d4d42d00e0c3418882ca579fc0
drec-stack <ruzalmuhamedgaleev@gmail.com>
2026-05-28 23:44:59 +0300

    Update player.js

--- js/player.js
+++ js/player.js
@@ -1,13 +1,21 @@
 (function() {
     'use strict';
     function PlayerManager() { this.audio=null; this.track=null; this.playing=false; this.volume=0.7; this.timer=null; this.events=[]; }
+    
     PlayerManager.prototype.init = function() {
         this.audio=new Audio(); this.audio.volume=this.volume; var s=this;
         this.audio.addEventListener('play',function(){s.playing=true;s.startTimer();s.emit('play');});
         this.audio.addEventListener('pause',function(){s.playing=false;s.stopTimer();s.emit('pause');});
         this.audio.addEventListener('ended',function(){s.playing=false;s.stopTimer();s.emit('ended');});
         this.audio.addEventListener('error',function(e){s.emit('error',e);});
+        // Подключение визуализатора
+        setTimeout(function() {
+            if(window.visualizer && s.audio) {
+                window.visualizer.connect(s.audio);
+            }
+        }, 100);
     };
+    
     PlayerManager.prototype.play = function(t) {
         if(!t)return; if(this.track&&this.track.id===t.id){this.toggle();return;} this.track=t; var s=this;
         var pu=function(u){s.audio.src=u;s.audio.play().catch(function(){});s.afterPlay();};
@@ -17,10 +25,12 @@
         else if(t.sourceUrl)pu(t.sourceUrl);
         else this.emit('no_source');
     };
+    
     PlayerManager.prototype.afterPlay = function() {
         if(this.track&&this.track.id){db.getTrack(this.track.id).then(function(tr){if(tr)db.updateTrack(tr.id,{playCount:(tr.playCount||0)+1,lastPlayed:Date.now()});}).catch(function(){});}
         storage.addToHistory(this.track); this.emit('track_change',this.track);
     };
+    
     PlayerManager.prototype.toggle = function() { if(!this.audio)return; if(this.playing)this.audio.pause();else this.audio.play().catch(function(){}); };
     PlayerManager.prototype.seek = function(t) { if(this.audio)this.audio.currentTime=t; };
     PlayerManager.prototype.getTime = function() { return this.audio?this.audio.currentTime:0; };
@@ -32,5 +42,6 @@
     PlayerManager.prototype.on = function(e,c) { this.events.push({e:e,cb:c}); };
     PlayerManager.prototype.emit = function(e,d) { for(var i=0;i<this.events.length;i++){if(this.events[i].e===e)this.events[i].cb(d);} };
     PlayerManager.prototype.fmt = function(s) { if(!s||isNaN(s))return'0:00'; var m=Math.floor(s/60),sec=Math.floor(s%60); return m+':'+(sec<10?'0':'')+sec; };
+    
     window.player = new PlayerManager();
 })();
