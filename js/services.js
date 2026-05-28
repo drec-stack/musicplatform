a30a7b733022a576ebd7760593cd6969a5d3653b
drec-stack <ruzalmuhamedgaleev@gmail.com>
2026-05-28 21:39:45 +0300

    Update services.js

--- js/services.js
+++ js/services.js
@@ -1,126 +1,33 @@
 (function() {
     'use strict';
-
     function ServicesManager() {
-        this.services = {
-            spotify: { id: 'spotify', name: 'Spotify', color: '#1DB954', authType: 'oauth', connected: false },
-            youtube: { id: 'youtube', name: 'YouTube Music', color: '#FF0000', authType: 'api_key', connected: false },
-            soundcloud: { id: 'soundcloud', name: 'SoundCloud', color: '#FF5500', authType: 'api_key', connected: false },
-            deezer: { id: 'deezer', name: 'Deezer', color: '#00C7F2', authType: 'none', connected: true }
+        this.items = {
+            spotify: {id:'spotify',name:'Spotify',color:'#1DB954',connected:false},
+            youtube: {id:'youtube',name:'YouTube Music',color:'#FF0000',connected:false},
+            soundcloud: {id:'soundcloud',name:'SoundCloud',color:'#FF5500',connected:false},
+            deezer: {id:'deezer',name:'Deezer',color:'#00C7F2',connected:true}
         };
-        this.loadState();
+        this.load();
     }
-
-    ServicesManager.prototype.loadState = function() {
-        var spotifyAuth = storage.get('spotify_auth');
-        if (spotifyAuth && spotifyAuth.accessToken && spotifyAuth.expiresAt > Date.now()) {
-            this.services.spotify.connected = true;
-            this.services.spotify.accessToken = spotifyAuth.accessToken;
-        }
-
-        var youtubeConfig = storage.get('youtube_config');
-        if (youtubeConfig && youtubeConfig.apiKey) {
-            this.services.youtube.connected = true;
-            this.services.youtube.apiKey = youtubeConfig.apiKey;
-        }
-
-        var soundcloudConfig = storage.get('soundcloud_config');
-        if (soundcloudConfig && soundcloudConfig.clientId) {
-            this.services.soundcloud.connected = true;
-            this.services.soundcloud.clientId = soundcloudConfig.clientId;
-        }
+    ServicesManager.prototype.load = function() {
+        var sa=storage.get('spotify_auth'); if(sa&&sa.accessToken&&sa.expiresAt>Date.now()){this.items.spotify.connected=true;this.items.spotify.accessToken=sa.accessToken;}
+        var yt=storage.get('youtube_config'); if(yt&&yt.apiKey){this.items.youtube.connected=true;this.items.youtube.apiKey=yt.apiKey;}
+        var sc=storage.get('soundcloud_config'); if(sc&&sc.clientId){this.items.soundcloud.connected=true;this.items.soundcloud.clientId=sc.clientId;}
     };
-
-    ServicesManager.prototype.getAll = function() {
-        var result = [];
-        for (var key in this.services) {
-            if (this.services.hasOwnProperty(key)) {
-                result.push(this.services[key]);
-            }
-        }
-        return result;
+    ServicesManager.prototype.getAll = function() { var r=[]; for(var k in this.items){if(this.items.hasOwnProperty(k))r.push(this.items[k]);} return r; };
+    ServicesManager.prototype.connectSpotify = function(cid) {
+        var rd=window.location.origin+'/callback.html', sc='streaming user-read-email user-read-private user-library-read playlist-read-private', st='', ch='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        for(var i=0;i<16;i++)st+=ch.charAt(Math.floor(Math.random()*ch.length));
+        storage.set('spotify_auth_state',st);
+        window.location.href='https://accounts.spotify.com/authorize?client_id='+encodeURIComponent(cid)+'&response_type=token&redirect_uri='+encodeURIComponent(rd)+'&scope='+encodeURIComponent(sc)+'&state='+st;
     };
-
-    ServicesManager.prototype.getConnected = function() {
-        var result = [];
-        for (var key in this.services) {
-            if (this.services.hasOwnProperty(key) && this.services[key].connected) {
-                result.push(this.services[key]);
-            }
-        }
-        return result;
+    ServicesManager.prototype.checkSpotifyCallback = function() { var sa=storage.get('spotify_auth'); if(sa&&sa.accessToken&&sa.expiresAt>Date.now()){this.items.spotify.connected=true;this.items.spotify.accessToken=sa.accessToken;return true;} return false; };
+    ServicesManager.prototype.connectYouTube = function(k) { storage.set('youtube_config',{apiKey:k}); this.items.youtube.connected=true; this.items.youtube.apiKey=k; };
+    ServicesManager.prototype.connectSoundCloud = function(c) { storage.set('soundcloud_config',{clientId:c}); this.items.soundcloud.connected=true; this.items.soundcloud.clientId=c; };
+    ServicesManager.prototype.disconnect = function(id) {
+        if(id==='spotify'){storage.remove('spotify_auth');this.items.spotify.connected=false;delete this.items.spotify.accessToken;}
+        if(id==='youtube'){storage.remove('youtube_config');this.items.youtube.connected=false;delete this.items.youtube.apiKey;}
+        if(id==='soundcloud'){storage.remove('soundcloud_config');this.items.soundcloud.connected=false;delete this.items.soundcloud.clientId;}
     };
-
-    ServicesManager.prototype.getActiveServiceTokens = function() {
-        var tokens = {};
-        if (this.services.spotify.connected) tokens.spotify = this.services.spotify.accessToken;
-        if (this.services.youtube.connected) tokens.youtube = this.services.youtube.apiKey;
-        if (this.services.soundcloud.connected) tokens.soundcloud = this.services.soundcloud.clientId;
-        if (this.services.deezer.connected) tokens.deezer = true;
-        return tokens;
-    };
-
-    ServicesManager.prototype.connectSpotify = function(clientId) {
-        var redirectUri = window.location.origin + '/callback.html';
-        var scopes = 'streaming user-read-email user-read-private user-library-read user-library-modify playlist-read-private';
-        var state = this.generateState();
-        storage.set('spotify_auth_state', state);
-        var params = 'client_id=' + encodeURIComponent(clientId) +
-            '&response_type=token' +
-            '&redirect_uri=' + encodeURIComponent(redirectUri) +
-            '&scope=' + encodeURIComponent(scopes) +
-            '&state=' + encodeURIComponent(state);
-        window.location.href = 'https://accounts.spotify.com/authorize?' + params;
-    };
-
-    ServicesManager.prototype.checkSpotifyCallback = function() {
-        var auth = storage.get('spotify_auth');
-        if (auth && auth.accessToken && auth.expiresAt > Date.now()) {
-            this.services.spotify.connected = true;
-            this.services.spotify.accessToken = auth.accessToken;
-            return true;
-        }
-        return false;
-    };
-
-    ServicesManager.prototype.connectYouTube = function(apiKey) {
-        storage.set('youtube_config', { apiKey: apiKey });
-        this.services.youtube.connected = true;
-        this.services.youtube.apiKey = apiKey;
-        return true;
-    };
-
-    ServicesManager.prototype.connectSoundCloud = function(clientId) {
-        storage.set('soundcloud_config', { clientId: clientId });
-        this.services.soundcloud.connected = true;
-        this.services.soundcloud.clientId = clientId;
-        return true;
-    };
-
-    ServicesManager.prototype.disconnect = function(serviceId) {
-        if (serviceId === 'spotify') {
-            storage.remove('spotify_auth');
-            this.services.spotify.connected = false;
-            delete this.services.spotify.accessToken;
-        } else if (serviceId === 'youtube') {
-            storage.remove('youtube_config');
-            this.services.youtube.connected = false;
-            delete this.services.youtube.apiKey;
-        } else if (serviceId === 'soundcloud') {
-            storage.remove('soundcloud_config');
-            this.services.soundcloud.connected = false;
-            delete this.services.soundcloud.clientId;
-        }
-    };
-
-    ServicesManager.prototype.generateState = function() {
-        var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        var result = '';
-        for (var i = 0; i < 16; i++) {
-            result += chars.charAt(Math.floor(Math.random() * chars.length));
-        }
-        return result;
-    };
-
     window.services = new ServicesManager();
 })();
