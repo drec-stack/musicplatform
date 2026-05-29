8add94255295722364844d66c5e7b9e7cab79a91
drec-stack <ruzalmuhamedgaleev@gmail.com>
2026-05-29 17:54:42 +0300

    Update crossfade.js

--- js/crossfade.js
+++ js/crossfade.js
@@ -2,122 +2,79 @@
     'use strict';
     
     function CrossfadeManager() {
-        this.enabled = true;
-        this.duration = 2000; // 2 секунды
-        this.nextAudio = null;
-        this.isCrossfading = false;
-        this.loadSettings();
-        this.patchPlayer();
+        this.enabled = false;
+        this.duration = 2000;
+        this.fadeOutDuration = 1500;
+        this.fadeInDuration = 1500;
+        this.isFading = false;
     }
     
-    CrossfadeManager.prototype.loadSettings = function() {
-        var saved = storage.get('crossfade_enabled');
-        if (saved !== undefined) this.enabled = saved;
-        var savedDuration = storage.get('crossfade_duration');
-        if (savedDuration) this.duration = savedDuration;
+    CrossfadeManager.prototype.enable = function() {
+        this.enabled = true;
+        if (window.ui) ui.notify('Crossfade enabled', 'success');
     };
     
-    CrossfadeManager.prototype.saveSettings = function() {
-        storage.set('crossfade_enabled', this.enabled);
-        storage.set('crossfade_duration', this.duration);
-    };
-    
-    CrossfadeManager.prototype.patchPlayer = function() {
-        var self = this;
-        var originalPlay = player.play;
-        
-        player.play = function(track) {
-            if (!self.enabled || !player.playing || !player.audio) {
-                return originalPlay.call(this, track);
-            }
-            
-            // Кроссфейд активен
-            self.startCrossfade(track, function() {
-                originalPlay.call(player, track);
-            });
-        };
-    };
-    
-    CrossfadeManager.prototype.startCrossfade = function(newTrack, callback) {
-        if (this.isCrossfading) return;
-        
-        this.isCrossfading = true;
-        var oldAudio = player.audio;
-        var oldVolume = player.volume;
-        
-        // Создаём новый аудиоэлемент для следующего трека
-        this.nextAudio = new Audio();
-        this.nextAudio.volume = 0;
-        
-        // Загружаем следующий трек
-        var self = this;
-        var loadNext = function() {
-            if (newTrack.isLocal) {
-                db.getAudioFile(newTrack.id).then(function(f) {
-                    if (f && f.blob) {
-                        self.nextAudio.src = URL.createObjectURL(f.blob);
-                        self.nextAudio.load();
-                        self.nextAudio.addEventListener('canplay', startFade);
-                    }
-                });
-            } else if (newTrack.preview) {
-                self.nextAudio.src = newTrack.preview;
-                self.nextAudio.load();
-                self.nextAudio.addEventListener('canplay', startFade);
-            } else if (newTrack.streamUrl) {
-                self.nextAudio.src = newTrack.streamUrl;
-                self.nextAudio.load();
-                self.nextAudio.addEventListener('canplay', startFade);
-            } else {
-                callback();
-                self.isCrossfading = false;
-            }
-        };
-        
-        var startFade = function() {
-            var startTime = Date.now();
-            var fadeStep = 20; // шаг в мс
-            var steps = self.duration / fadeStep;
-            var volumeStepOld = oldVolume / steps;
-            var volumeStepNew = 1 / steps;
-            
-            var interval = setInterval(function() {
-                var elapsed = Date.now() - startTime;
-                var progress = Math.min(1, elapsed / self.duration);
-                
-                if (oldAudio) oldAudio.volume = Math.max(0, oldVolume - (progress * oldVolume));
-                if (self.nextAudio) self.nextAudio.volume = progress;
-                
-                if (progress >= 1) {
-                    clearInterval(interval);
-                    oldAudio.pause();
-                    oldAudio.volume = oldVolume;
-                    
-                    // Переключаемся на новый аудио
-                    player.audio = self.nextAudio;
-                    player.audio.volume = oldVolume;
-                    player.audio.play();
-                    
-                    self.nextAudio = null;
-                    self.isCrossfading = false;
-                    callback();
-                }
-            }, fadeStep);
-        };
-        
-        loadNext();
-    };
-    
-    CrossfadeManager.prototype.toggle = function() {
-        this.enabled = !this.enabled;
-        this.saveSettings();
-        if (window.ui) ui.notify(this.enabled ? '🎵 Crossfade on' : '🎵 Crossfade off', 'info');
+    CrossfadeManager.prototype.disable = function() {
+        this.enabled = false;
+        if (window.ui) ui.notify('Crossfade disabled', 'info');
     };
     
     CrossfadeManager.prototype.setDuration = function(ms) {
-        this.duration = Math.min(5000, Math.max(500, ms));
-        this.saveSettings();
-        if (window.ui) ui.notify('Crossfade duration: ' + (this.duration / 1000) + 's', 'info');
+        this.duration = Math.max(500, Math.min(10000, ms));
+        this.fadeOutDuration = this.duration * 0.7;
+        this.fadeInDuration = this.duration * 0.3;
+    };
+    
+    CrossfadeManager.prototype.apply = function(oldTrack, newTrack) {
+        var self = this;
+        
+        if (!this.enabled || !window.player) {
+            if (window.player) player.play(newTrack);
+            return;
+        }
+        
+        this.isFading = true;
+        
+        var startVolume = player.volume;
+        var fadeOutStart = Date.now();
+        
+        function fadeOut() {
+            var elapsed = Date.now() - fadeOutStart;
+            var progress = Math.min(1, elapsed / self.fadeOutDuration);
+            var volume = startVolume * (1 - progress);
+            player.setVolume(Math.max(0, volume));
+            
+            if (progress < 1) {
+                requestAnimationFrame(fadeOut);
+            } else {
+                player.setVolume(0);
+                player.play(newTrack);
+                
+                var fadeInStart = Date.now();
+                
+                function fadeIn() {
+                    var elapsed = Date.now() - fadeInStart;
+                    var progress = Math.min(1, elapsed / self.fadeInDuration);
+                    var volume = startVolume * progress;
+                    player.setVolume(volume);
+                    
+                    if (progress < 1) {
+                        requestAnimationFrame(fadeIn);
+                    } else {
+                        player.setVolume(startVolume);
+                        self.isFading = false;
+                    }
+                }
+                
+                requestAnimationFrame(fadeIn);
+            }
+        }
+        
+        requestAnimationFrame(fadeOut);
+    };
+    
+    CrossfadeManager.prototype.isEnabled = function() {
+        return this.enabled;
     };
     
     window.crossfade = new CrossfadeManager();
